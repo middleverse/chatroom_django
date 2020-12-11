946083f10ba5ce218f5fbcec08213f89ba41e096
middleverse <arshi_b@hotmail.com>
2020-12-11 18:59:13 -0500

    tester message sends to base viwe functionality

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,4 +1,33 @@
+import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatRoomConsumer(AsyncWebsocketConsumer):
-    pass
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+        
+        await self.channel_layer.group_add(
+            self.room_group_name, 
+            self.channel_name # a pointer to the channel layer's channel name
+        )
+
+        await self.channel_layer.group_send( # send a message to the group as soon as we connect
+            self.room_group_name,
+            {
+                'type' : 'tester message',
+                'tester' : 'hello_universe',
+            }
+        )
+    
+    async def tester_message(self, event):
+        tester = event['tester']
+
+        await self.send(text_data=json.dumps({
+            'tester' : tester,
+        }))
+
+    async def disconnect(self):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name    
+        )
--- chat/views.py
+++ chat/views.py
@@ -4,6 +4,6 @@ def index(request):
     return render(request, 'index.html', {})
 
 def room(request, room_name):
-    return render(request, 'chatroom.html', {
+    return render(request, 'base.html', {
         'room_name': room_name
     })
